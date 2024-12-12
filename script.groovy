52a4a512f5ac8820aa1404647f728c391b436687
Marek Wodawski <wodawskimarek@gmail.com>
2024-12-12 13:35:13 +0100

    another

--- script.groovy
+++ script.groovy
@@ -1,5 +1,14 @@
-def function() {
+def buildApp() {
     echo 'building the application ....'
+}
+
+def testApp() {
+    echo 'testing the application ....'
+}
+
+def deployApp() {
+    echo 'deployinjg the app'
+    echo "deploying app version ${params.VERSION}"
 }
 
 return this
